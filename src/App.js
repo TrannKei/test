a84e0bf5eb5896e627c2416c9b407683fea6125a
TrannKei <trannkei1998@gmail.com>
2022-07-26 18:58:06 +0700

    huhu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react'
 function App() {
+  const [name, setName] = useState('')
+
+  const [data, setData] = useState([{}])
+  useEffect(() => {
+    async function getDataAPI() {
+      const getData = await fetch('https://dummyjson.com/todos')
+        .then(res => res.json())
+        .then(data => {
+          data.todos.map((item) => {
+            // item.time = time;
+            // item.date = date;
+            item.removed = false
+            item.completed = false
+            item.disabled = true
+            item.isEdit = false
+            item.addOn =[{title:"54"},{title:"54"},{title:"54"},{title:"54"},{title:"54"}]
+            return item
+          }
+          )
+          setData(data.todos)
+
+        })
+      // .then(()=>{
+
+      // })
+
+
+    }
+    getDataAPI()
+
+  }, [])
+  // const [input, setInput] = useState([{}])
+  // data[1].addOn = {title:"haw"}
+  // console.log(data[1]);
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data.map((item)=>{
+        {item.addOn.map((ele)=>{
+          <p>{ele.title}</p>
+        })}
+      })}
+    </>
   );
 }
 
